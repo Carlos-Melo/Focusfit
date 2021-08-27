cde58cf25c2aeebcfe2a2656328959cfd80b9724
Carlos-Melo <carloszdu@gmail.com>
2021-08-27 17:02:12 -0300

    imc-cacl

--- main.js
+++ main.js
@@ -14,3 +14,30 @@ new Glider($simpleCarousel2, {
     draggable: true,
     dots: ".js-carousel--simple-dots2",
 });
+
+//IMC
+function imcCalc() {
+
+    let peso = document.querySelector('input[name=peso]').value;
+    let altura = document.querySelector('input[name=altura]').value;
+    let result = document.querySelector('#result');
+
+    if (peso != '' && altura != '') {
+        p = parseFloat(peso.replace(',', '.'));
+        a = parseFloat(altura.replace(',', '.'));
+
+        let imc = p / Math.pow(a, 2);
+
+        if (isNaN(imc))
+            result.innerHTML = 'Digite o peso e altura corretamente';
+        else {
+            result.innerHTML = `Resultado: ${imc.toFixed(1)}`;
+
+            document.querySelector('input[name=peso]').value = '';
+            document.querySelector('input[name=altura]').value = '';
+        }
+
+    } else {
+        result.innerHTML = 'Digite o peso e altura corretamente';
+    }
+}
